6c0e7b754cb2387976bdedbefd31730742d95d27
NguyenManh <nguyendinhdinhad@gmail.com>
2026-05-06 17:11:16 +0700

    Nộp đơn ( application )

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import auth, application
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(application.router)
 
 @app.get("/")
 def root():
