66c74a1bb8a3b0ee8ebdd8eee4bb956159a92ba0
NguyenManh <nguyendinhdinhad@gmail.com>
2026-05-06 17:28:34 +0700

    fix main, application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from app.routers import auth, application
+
+from app.routers import auth, application, school, major,subject_group
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(application.router)
+app.include_router(school.router)
+app.include_router(major.router)
+app.include_router(subject_group.router)
 
 @app.get("/")
 def root():
