14e98f82872704d59a8130ef961c34ab3c35bba5
dungnv-ptit-2006 <dungnv.ptit.2006@gmail.com>
2026-05-06 17:26:30 +0700

    sửa file main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routers import auth, application
+from app.routers import auth, application, major, school, subject_group
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(application.router)
+app.include_router(major.router)
+app.include_router(school.router)   
+app.include_router(subject_group.router)
 
 @app.get("/")
 def root():
