a56666238f5cab0f798aa6eb93b84772ba14c111
dungnv-ptit-2006 <dungnv.ptit.2006@gmail.com>
2026-05-13 09:29:07 +0700

    stats

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import FileResponse
 
 from app.core.cloudinary_config import init_cloudinary
 
-from app.routers import auth, application, school, major, subject_group, file
+from app.routers import auth, application, school, major, subject_group, file, stats
 
 app = FastAPI()
 
@@ -15,7 +15,7 @@ app.include_router(school.router)
 app.include_router(major.router)
 app.include_router(subject_group.router)
 app.include_router(file.router)
-
+app.include_router(stats.router)
 @app.get("/favicon.ico")
 def favicon():
     return {}
---
+++ app/routers/stats.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from app.db.database import get_db
+
+from app.models.application import Application
+from app.models.school import School
+from app.models.major import Major
+
+from app.models.user import User
+from app.core.deps import require_admin
+
+router = APIRouter(
+    prefix="/stats",
+    tags=["Stats"]
+)
+
+
+@router.get("/")
+def get_stats(
+    db: Session = Depends(get_db),
+    admin: User = Depends(require_admin)
+):
+
+    #thống kê theo status
+    by_status = db.query(
+        Application.status,
+        func.count(Application.id).label("total")
+    ).group_by(
+        Application.status
+    ).all()
+
+    # thống kê theo school
+    by_school = db.query(
+        School.name,
+        func.count(Application.id).label("total")
+    ).join(
+        Application,
+        Application.school_id == School.id
+    ).group_by(
+        School.id
+    ).all()
+    
+    # thống kê theo major
+    by_major = db.query(
+        Major.name,
+        func.count(Application.id).label("total")
+    ).join(
+        Application,
+        Application.major_id == Major.id
+    ).group_by(
+        Major.id
+    ).all()
+
+    return {
+
+        "byStatus": [
+            {
+                "status": item.status,
+                "total": item.total
+            }
+            for item in by_status
+        ],
+
+        "bySchool": [
+            {
+                "school": item.name,
+                "total": item.total
+            }
+            for item in by_school
+        ],
+
+        "byMajor": [
+            {
+                "major": item.name,
+                "total": item.total
+            }
+            for item in by_major
+        ]
+    }
